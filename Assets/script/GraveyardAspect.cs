974d67e7f16300368eebafa49e9fe2bb465dbadb
DESKTOP-CDKVEGQ\admin <ejrduddlekt@naver.com>
2023-07-02 22:39:09 +0900

    System

--- Assets/script/GraveyardAspect.cs
+++ Assets/script/GraveyardAspect.cs
@@ -6,64 +6,13 @@ using UnityEngine;
 
 namespace TMG.Zombies
 {
-    public readonly partial struct GraveyardAspect : IAspect
+    public readonly partial struct GraveyardAspect : IAspect //Aspect를 쓰기 위해선 IAspect 인터페이스를 상속받아야 한다.
     {
-        public readonly Entity Entity;
+        public readonly Entity Entity; //Entity에 대한 참조
 
         private readonly RefRO<LocalTransform> _transform;
         private LocalTransform Transform => _transform.ValueRO;
         private readonly RefRO<GraveyardProperties> _graveyardProperties;
         private readonly RefRW<GraveyardRandom> _graveyardRandom;
-
     }
 }
-
-//
-
-//private readonly RefRO<GraveyardProperties> _graveyardProperties;
-//
-
-//public int NumberTombstonesToSpawn => _graveyardProperties.ValueRO.NumberTombstonesToSpawn;
-//public Entity TombstonePrefab => _graveyardProperties.ValueRO.TombstonePrefab;
-
-
-
-//public LocalTransform GetRandomTombstoneTransform()
-//{
-//    return new LocalTransform
-//    {
-//        Position = GetRandomPosition(),
-//        Rotation = GetRandomRotation(),
-//        Scale = GetRandomScale(0.5f)
-//    };
-//}
-
-//private float3 GetRandomPosition()
-//{
-//    float3 randomPosition;
-//    do
-//    {
-//        randomPosition = _graveyardRandom.ValueRW.Value.NextFloat3(MinCorner, MaxCorner);
-//    } while (math.distancesq(Transform.Position, randomPosition) <= BRAIN_SAFETY_RADIUS_SQ);
-
-//    return randomPosition;
-//}
-
-//private float3 MinCorner => Transform.Position - HalfDimensions;
-//private float3 MaxCorner => Transform.Position + HalfDimensions;
-//private float3 HalfDimensions => new()
-//{
-//    x = _graveyardProperties.ValueRO.FieldDimensions.x * 0.5f,
-//    y = 0f,
-//    z = _graveyardProperties.ValueRO.FieldDimensions.y * 0.5f
-//};
-//private const float BRAIN_SAFETY_RADIUS_SQ = 100;
-
-//private quaternion GetRandomRotation() => quaternion.RotateY(_graveyardRandom.ValueRW.Value.NextFloat(-0.25f, 0.25f));
-//private float GetRandomScale(float min) => _graveyardRandom.ValueRW.Value.NextFloat(min, 1f);
-
-//public float2 GetRandomOffset()
-//{
-//    return _graveyardRandom.ValueRW.Value.NextFloat2();
-//}
-
